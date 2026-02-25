5c3b32077759016b24b9c97186ca45bb7547555a
Chris <cmachado1@arizona.edu>
2026-02-25 16:31:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const ID = '218233568315310081';
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+    //console.log(message.author);
+
+    if (message.author.bot) return; // ignore other bots
+
+    if (message.author.id === ID){
+        //message.reply('Chris did not send this message')
+        message.react('🤡');
+    }
+
+    /*
+    if (message.content === '!hello') {
+        message.reply('Hello!');
+    }
+
+    if (message.content === '!jerm') {
+        message.reply('I am Jerami and I am Gay!');
+    }*/
+
+});
+
+client.login(process.env.DISCORD_TOKEN);
